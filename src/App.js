dc13e0926f33bff925c1633ea69c38cdd125bfc8
Shifatur-Rahman <rjnovel28@gmail.com>
2023-10-31 23:46:50 +0600

    img add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import img1 from "./assets/img/image1.jpg";
+import img2 from "./assets/img/image2.jpg";
+import img3 from "./assets/img/image3.jpg";
+// import img2 from "..//public/assets/img/image2.jpg"
+// import img3 from "..//public/assets/img/image3.jpg"
 
 function App() {
   return (
