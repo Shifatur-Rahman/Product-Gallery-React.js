3722e3733225c86cb4f2e7f16935fca3e7d9124a
Shifatur-Rahman <rjnovel28@gmail.com>
2023-11-01 00:37:34 +0600

    plugin add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import './App.css';
-import img1 from "./assets/img/image1.jpg";
+import img1 from "././assets/img/image1.jpg";
 import img2 from "./assets/img/image2.jpg";
-import img3 from "./assets/img/image3.jpg";
+import img3 from "././assets/img/image1.jpg";
 // import img2 from "..//public/assets/img/image2.jpg"
 // import img3 from "..//public/assets/img/image3.jpg"
 
 function App() {
+  const products = [
+    {
+      id:1,
+      name:"chair",
+      img:img1
+    },
+    {
+      id:2,
+      name:"table",
+      img:img2
+    },
+    {
+      id:1,
+      name:"desk",
+      img:img3
+    }
+  ]
   return (
-    <div className="App">
+    <>
      <h1>Hello world</h1>
-    </div>
+     {
+      products.map(singleProduct=>{
+        // console.log(singleProduct);
+        return(
+          <div className="novel"> 
+           <p>{singleProduct.name}</p>
+        <img src={process.env.PUBLIC_URL + singleProduct.img} alt="img" />
+        </div>
+        )
+        
+      })
+     }
+    </>
   );
 }
 
