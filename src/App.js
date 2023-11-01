074fd04bf43f5423c27c7cd7eb752deb4f64335c
Shifatur-Rahman <rjnovel28@gmail.com>
2023-11-01 09:55:56 +0600

    package add

--- src/App.js
+++ src/App.js
@@ -4,40 +4,76 @@ import img2 from "./assets/img/image2.jpg";
 import img3 from "././assets/img/image1.jpg";
 // import img2 from "..//public/assets/img/image2.jpg"
 // import img3 from "..//public/assets/img/image3.jpg"
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+
 
 function App() {
   const products = [
     {
       id:1,
       name:"chair",
-      img:img1
+      thumb:img1
     },
     {
       id:2,
       name:"table",
-      img:img2
+      thumb:img2
     },
     {
       id:1,
       name:"desk",
-      img:img3
+      thumb:img3
     }
   ]
   return (
     <>
-     <h1>Hello world</h1>
-     {
+     {/* {
       products.map(singleProduct=>{
-        // console.log(singleProduct);
         return(
-          <div className="novel"> 
-           <p>{singleProduct.name}</p>
-        <img src={process.env.PUBLIC_URL + singleProduct.img} alt="img" />
-        </div>
+          <DragDropContext>
+            <Droppable droppableId="characters">
+              <div className="novel">
+                <p>{singleProduct.name}</p>
+                <img src={process.env.PUBLIC_URL + singleProduct.img} alt="img" />
+              </div>
+            </Droppable>
+          </DragDropContext>
         )
         
       })
-     }
+     } */}
+
+
+
+
+
+<ul className="characters">
+  {products.map(({id, name, thumb}) => {
+    return (
+      <li key={id}>
+        <div className="characters-thumb">
+          <img src={thumb} alt={`${name} Thumb`} />
+        </div>
+        <p>
+          { name }
+        </p>
+      </li>
+    );
+  })}
+</ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
     </>
   );
 }
