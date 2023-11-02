439a44bf7a229d75eff4264627ae4755c268788a
Shifatur-Rahman <rjnovel28@gmail.com>
2023-11-02 08:15:40 +0600

    product image add

--- src/App.js
+++ src/App.js
@@ -7,48 +7,28 @@ import img3 from "././assets/img/image1.jpg";
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import Product from './components/Product';
 import { useState } from 'react';
-
+import webp1 from "././assets/images/image-1.webp";
 
 function App() {
   const products = [
     {
       id:1,
-      name:"chair",
-      thumb:img1
+      thumb:webp1
     },
     {
       id:2,
-      name:"table",
       thumb:img2
     },
     {
       id:1,
-      name:"desk",
       thumb:img3
     }
   ]
+
   return (
     <div className='App'>
-    
-     {/* {
-      products.map(singleProduct=>{
-        return(
-          <DragDropContext>
-            <Droppable droppableId="characters">
-              <div className="novel">
-                <p>{singleProduct.name}</p>
-                <img src={process.env.PUBLIC_URL + singleProduct.img} alt="img" />
-              </div>
-            </Droppable>
-          </DragDropContext>
-        )
-        
-      })
-     } */}
 
   <Product />
-
-
 
       {/* <DragDropContext>
         <Droppable droppableId="characters">
@@ -70,12 +50,10 @@ function App() {
                   </Draggable>
                 );
               })}
-
             </ul>
           )}
         </Droppable>
       </DragDropContext> */}
-
 
     </div>
   );
@@ -84,81 +62,3 @@ function App() {
 export default App;
 
 
-
-// const finalSpaceCharacters = [
-//   {
-//     id: 'gary',
-//     name: 'Gary Goodspeed',
-//     thumb: img1
-//   },
-//   {
-//     id: 'cato',
-//     name: 'Little Cato',
-//     thumb: img2
-//   },
-//   {
-//     id: 'kvn',
-//     name: 'KVN',
-//     thumb: img3
-//   },
-//   {
-//     id: 'mooncake',
-//     name: 'Mooncake',
-//     thumb: img1
-//   },
-//   {
-//     id: 'quinn',
-//     name: 'Quinn Ergon',
-//     thumb: img2
-//   }
-// ]
-
-// function App() {
-//   const [characters, updateCharacters] = useState(finalSpaceCharacters);
-
-//   function handleOnDragEnd(result) {
-//     if (!result.destination) return;
-//     const items = Array.from(characters);
-//     const [reorderedItem] = items.splice(result.source.index, 1);
-//     items.splice(result.destination.index, 0, reorderedItem);
-//     updateCharacters(items);
-//   }
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Final Space Characters</h1>
-//         <DragDropContext onDragEnd={handleOnDragEnd}>
-//           <Droppable droppableId="characters">
-//             {(provided) => (
-//               <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
-//                 {characters.map(({id, name, thumb}, index) => {
-//                   return (
-//                     <Draggable key={id} draggableId={id} index={index}>
-//                       {(provided) => (
-//                         <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-//                           <div className="characters-thumb">
-//                             <img src={thumb} alt={`${name} Thumb`} />
-//                           </div>
-//                           <p>
-//                             { name }
-//                           </p>
-//                         </li>
-//                       )}
-//                     </Draggable>
-//                   );
-//                 })}
-//                 {provided.placeholder}
-//               </ul>
-//             )}
-//           </Droppable>
-//         </DragDropContext>
-//       </header>
-//       <p>
-//         Images from <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">Final Space Wiki</a>
-//       </p>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,76 +1,33 @@
-import React, {useState} from 'react'
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import img1 from "..//assets/img/image1.jpg";
-import img2 from "..//assets/img/image2.jpg";
-import img3 from "..//assets/img/image3.jpg";
+import React from 'react'
+import image1 from "../assets/images/image-1.webp"
+import image2 from "../assets/images/image-2.webp"
+import image3 from "../assets/images/image-3.webp"
+import image4 from "../assets/images/image-4.webp"
+import image5 from "../assets/images/image-5.webp"
+import image6 from "../assets/images/image-6.webp"
+import image7 from "../assets/images/image-7.webp"
+import image8 from "../assets/images/image-8.webp"
+import image9 from "../assets/images/image-9.webp"
+import image10 from "../assets/images/image-10.jpeg"
+import image11 from "../assets/images/image-11.jpeg"
 
+import productData from "../../src/data/Products.json"
 
-const Product = () => {
-
-    const products = [
-        {
-          id:1,
-          name:"chair",
-          thumb:img1
-        },
-        {
-          id:2,
-          name:"table",
-          thumb:img2
-        },
-        {
-          id:1,
-          name:"desk",
-          thumb:img3
-        }
-      ]
-
-      const [characters, updateCharacters] = useState(products);
-
-    function handleOnDragEnd(result) {
-        if (!result.destination) return;
-        const items = Array.from(characters);
-        const [reorderedItem] = items.splice(result.source.index, 1);
-        items.splice(result.destination.index, 0, reorderedItem);
-        updateCharacters(items);
-    }
-
+const Product = ({data}) => {
   return (
     <>
     
-    <div className="app">
-      <header className="App-header">
-        <h1>Final Space Characters</h1>
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="characters">
-            {(provided) => (
-              <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
-                {characters.map(({id, name, thumb}, index) => {
-                  return (
-                    <Draggable key={id} draggableId={id} index={index}>
-                      {(provided) => (
-                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                          <div className="characters-thumb">
-                            <img src={process.env.PUBLIC_URL + thumb} alt={`${name} Thumb`} />
-                          </div>
-                          <p>
-                            { name }
-                          </p>
-                        </li>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </header>
-      <p>
-        Images from <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">Final Space Wiki</a>
-      </p>
-    </div>
+    {
+       productData.map(singleProduct=>{
+        return(
+            <div>
+            <p>{singleProduct.id}</p> 
+            <img src={singleProduct.thumb} />
+            <img src={process.env.PUBLIC_URL + singleProduct.thumb} />
+            </div>
+        )
+       }) 
+    }
     
     
     
